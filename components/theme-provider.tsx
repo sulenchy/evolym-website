4174291d80758b9edc5c721f81e920c4026b52cf
sulenchy <sulaimanabiodun172@gmail.com>
2025-07-11 23:11:10 +0100

    fix the ThemeProvider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,19 +1,14 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-"use client"
-
-import { useEffect, useState } from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // Avoid hydration mismatch by not rendering until mounted
-  const [mounted, setMounted] = useState(false)
+export function ThemeProvider({ children, ...props }: any) {
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  // Only render the children when mounted to avoid hydration mismatch
-  return <NextThemesProvider {...props}>{mounted ? children : null}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{mounted ? children : null}</NextThemesProvider>;
 }
